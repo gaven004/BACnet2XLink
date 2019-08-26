d670f33469af547b3bb5dfc9d80cb7fe1b51771b
Gaven <gaven_yeung@163.com>
2019-08-26 16:41:47 +0800

    完善框架，加入任务异常时，重新初始化的处理

--- src/main/java/com/g/bacnet2xlink/Main.java
+++ src/main/java/com/g/bacnet2xlink/Main.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import com.serotonin.bacnet4j.LocalDevice;
@@ -29,21 +30,40 @@ public class Main {
 
     public static final String ConfigFile = "/config.json";
 
+    private static Configuration cfg = null;
+    private static List<ObjectIdentifier> readOids = new ArrayList<>();
+
+    private LocalDevice localDevice = null;
+    private RemoteDevice remoteDevice = null;
+
+    private ScheduledExecutorService executor;
+
     public static void main(String[] args) {
         log.info("启动系统...");
 
         // 读系统配置
-        Configuration cfg = null;
+        config();
+
+        // 初始化
+        Main me = new Main();
+
+        // 注册退出钩子
+        me.addShutdownHook();
+
+        // 运行任务
+        me.runTask();
+    }
+
+    private static void config() {
         try {
             log.info("读系统配置...");
             cfg = Configuration.fromResource(ConfigFile);
-        } catch (IOException e) {
+        } catch (Exception e) {
             log.error("从资源文件恢复系统配置失败，请检查资源文件是否在指定路径，并且格式正确", e);
             log.error("退出系统！！！");
             System.exit(1);
         }
 
-        List<ObjectIdentifier> readOids = new ArrayList<>();
         try {
             for (Configuration.ObjectCfg obj : cfg.getDevice().getReadObjs()) {
                 readOids.add(new ObjectIdentifier(ObjectType.forName(obj.getObjectType()), obj.getObjectId()));
@@ -53,67 +73,6 @@ public class Main {
             log.error("退出系统！！！");
             System.exit(1);
         }
-
-        // 初始化
-        IpNetworkBuilder builder;
-        IpNetwork network;
-        LocalDevice localDevice = null;
-        RemoteDevice remoteDevice = null;
-        try {
-            log.info("初始化localDevice...");
-            builder = new IpNetworkBuilder();
-            if (StringUtils.isNotBlank(cfg.getLocalAddress())) {
-                builder.withLocalBindAddress(cfg.getLocalAddress());
-            }
-            if (cfg.getLocalPort() != null && cfg.getLocalPort() != 0) {
-                builder.withPort(cfg.getLocalPort());
-            }
-            builder.withBroadcast("192.168.0.255", 24);
-            network = builder.build();
-            localDevice = new LocalDevice(1, new DefaultTransport(network));
-            localDevice.initialize();
-            log.info("初始化localDevice成功：{}", localDevice);
-        } catch (Exception e) {
-            log.error("初始化localDevice失败", e);
-            log.error("退出系统！！！");
-            System.exit(1);
-        }
-
-        while (remoteDevice == null) {
-            try {
-                log.info("连接remoteDevice...");
-                remoteDevice = localDevice.getRemoteDeviceBlocking(cfg.getDevice().getDeviceId(), 10000);
-                getExtendedDeviceInformation(localDevice, remoteDevice);
-                log.info("连接remoteDevice成功");
-            } catch (BACnetException e) {
-                log.error("连接remoteDevice失败", e);
-                log.error("清空localDevice缓存，1分钟后重试");
-                localDevice.clearRemoteDevices();
-                try {
-                    Thread.sleep(60000);
-                } catch (InterruptedException ex) {
-                }
-            }
-        }
-
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
-
-        // 启动监听线程，接收控制指令
-        DataAcquisitionTask dat = new DataAcquisitionTask(localDevice, remoteDevice, readOids);
-        executor.scheduleWithFixedDelay(dat, cfg.getDevice().getDataSubmitInterval(), cfg.getDevice().getDataSubmitInterval(), TimeUnit.SECONDS);
-
-        // 启动上报线程，采集并上报数据
-
-        try {
-//            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
-            executor.awaitTermination(1, TimeUnit.MINUTES);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        executor.shutdown();
-        localDevice.terminate();
-        log.info("退出系统！！！");
     }
 
     /**
@@ -134,5 +93,114 @@ public class Main {
                 log.info(String.format("\t%s = %s", opr.getPropertyIdentifier().toString(), pvs.getNoErrorCheck(opr)));
             }
         }
+    }
+
+    private void initLocalDevice() {
+        IpNetworkBuilder builder;
+        IpNetwork network;
+        try {
+            log.info("初始化localDevice...");
+            builder = new IpNetworkBuilder();
+            if (StringUtils.isNotBlank(cfg.getLocalAddress())) {
+                builder.withLocalBindAddress(cfg.getLocalAddress());
+            }
+            if (cfg.getLocalPort() != null && cfg.getLocalPort() != 0) {
+                builder.withPort(cfg.getLocalPort());
+            }
+            builder.withBroadcast("192.168.0.255", 24);
+            network = builder.build();
+            localDevice = new LocalDevice(1, new DefaultTransport(network));
+            localDevice.initialize();
+            log.info("初始化localDevice成功：{}", localDevice);
+        } catch (Exception e) {
+            log.error("初始化localDevice失败", e);
+            log.error("退出系统！！！");
+            System.exit(1);
+        }
+    }
+
+    private void initRemoteDevice() {
+        while (remoteDevice == null) {
+            try {
+                log.info("连接remoteDevice...");
+                remoteDevice = localDevice.getRemoteDeviceBlocking(cfg.getDevice().getDeviceId(), 10000);
+                getExtendedDeviceInformation(localDevice, remoteDevice);
+                log.info("连接remoteDevice成功");
+            } catch (BACnetException e) {
+                log.error("连接remoteDevice失败", e);
+                log.error("清空localDevice缓存，1分钟后重试");
+                localDevice.clearRemoteDevices();
+                try {
+                    Thread.sleep(60000);
+                } catch (InterruptedException ex) {
+                }
+            }
+        }
+    }
+
+    private void initThreadPool() {
+        executor = Executors.newScheduledThreadPool(2);
+    }
+
+    private void runTask() {
+        while (true) {
+            initLocalDevice();
+            initRemoteDevice();
+            initThreadPool();
+
+            // 启动监听线程，接收控制指令
+
+            // 启动上报线程，采集并上报数据
+            DataAcquisitionTask dat = new DataAcquisitionTask(localDevice, remoteDevice, readOids);
+            final ScheduledFuture<?> daf = executor.scheduleWithFixedDelay(dat,
+                    cfg.getDevice().getDataSubmitInterval(), cfg.getDevice().getDataSubmitInterval(), TimeUnit.SECONDS);
+
+            while (!daf.isDone() && !daf.isCancelled()) {
+                try {
+                    Thread.sleep(60000);
+                } catch (InterruptedException e) {
+                }
+            }
+
+            // 任务中有异常抛出，则任务结束，重新初始化设备，并运行任务
+            renew();
+        }
+    }
+
+    void shutdown() {
+        log.info("关闭定时任务和设备");
+        if (executor != null) {
+            executor.shutdown();
+            try {
+                executor.awaitTermination(1, TimeUnit.MINUTES);
+            } catch (InterruptedException e) {
+            }
+        }
+        if (localDevice != null) {
+            localDevice.terminate();
+        }
+    }
+
+    private void release() {
+        executor = null;
+        remoteDevice = null;
+        localDevice = null;
+    }
+
+    void renew() {
+        log.warn("重新初始化设备，并运行任务");
+        shutdown();
+        release();
+    }
+
+    private void addShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                shutdown();
+                release();
+                log.info("退出系统！！！");
+            }
+        });
     }
 }
